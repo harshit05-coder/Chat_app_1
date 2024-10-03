6abd23d4d35c57b3e5a9914df8fdbbbd2f974017
Harshit Singh <harshitsingh05oct@gmail.com>
2024-10-03 22:13:25 +0530

    first

---
+++ chat-App-3/frontend/src/Pages/Homepage.js
@@ -0,0 +1,62 @@
+ import React from 'react'
+import {
+    Box,
+    Container,
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
+    Text,
+  } from "@chakra-ui/react";
+   import { useEffect } from "react";
+   import { useNavigate } from "react-router";
+   import Login from "../components/Authentication/Login";
+   import Signup from "../components/Authentication/Signup";
+  
+  function Homepage() {
+     const navigate = useNavigate();
+  
+    useEffect(() => {
+      const user = JSON.parse(localStorage.getItem("userInfo"));
+  
+      if (user) navigate("/chats");
+    }, [navigate]);
+  
+    return (
+      <Container maxW="xl" centerContent>
+        <Box
+          display="flex"
+          justifyContent="center"
+          padding={3}
+          background="white"
+          width="100%"
+          margin="40px 0 15px 0"
+          borderRadius="lg"
+          borderWidth="1px"
+        >
+          <Text fontSize="4xl" fontFamily="Work sans" color="black">
+            Chat_App
+          </Text>
+        </Box>
+        <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+          <Tabs isFitted variant="soft-rounded">
+            <TabList mb="1em">
+              <Tab width="50%">Login</Tab>
+              <Tab width="50%">Sign Up</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <Login />
+              </TabPanel>
+              <TabPanel>
+                <Signup />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      </Container>
+    );
+  }
+  
+  export default Homepage;
